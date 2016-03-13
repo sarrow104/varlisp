068179fa0fbc7bb4d85d6e07fb11b7867f9ba504
sarrow104 <sarrow104@gmail.com>
2016-03-13 11:27:47 +0800

    continuous interpret

--- varlisp/interpreter.cpp
+++ varlisp/interpreter.cpp
@@ -3,6 +3,7 @@
 #include "builtin.hpp"
 
 #include <sss/regex/cregex.hpp>
+#include <sss/log.hpp>
 
 #include <memory>
 
@@ -15,6 +16,7 @@ namespace varlisp {
 
     Interpreter::status_t Interpreter::eval(const std::string& line)
     {
+        SSS_LOG_EXPRESSION(sss::log::log_DEBUG, line);
         status_t ret = status_OK;
 
         static sss::regex::CRegex reg_quit("^\\s*\\(\\s*quit\\s*\\)\\s*$");
@@ -23,11 +25,23 @@ namespace varlisp {
         }
         else {
             // varlisp::Parser parser(line);
-            m_parser.parse(this->m_env, line);
+            int ec = m_parser.parse(this->m_env, line);
+            if (!ec) {
+                ret = status_UNFINISHED;
+            }
+            else if (ec < 0) {
+                ret = status_ERROR;
+            }
         }
 
-
         return ret;
+    }
+
+    int Interpreter::retrieve_symbols(std::vector<std::string>& symbols) const
+    {
+        for (Environment::const_iterator it = m_env.begin(); it != m_env.end(); ++it) {
+            symbols.push_back(it->first);
+        }
     }
 
 } // namespace varlisp
