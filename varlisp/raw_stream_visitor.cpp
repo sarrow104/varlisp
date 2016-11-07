cc7734b1658ca5dbf68e2385b3945982d28257d3
sarrow104 <sarrow104@gmail.com>
2016-11-07 11:33:54 +0800

    add raw_stream_vsitor for std::ofstream output

---
+++ varlisp/raw_stream_visitor.cpp
@@ -0,0 +1,18 @@
+#include "raw_stream_visitor.hpp"
+
+#include "environment.hpp"
+#include <sss/util/PostionThrow.hpp>
+
+#include <stdexcept>
+
+namespace varlisp {
+void raw_stream_visitor::operator()(const varlisp::symbol& s) const
+{
+    Object* it = m_env.find(s.m_data);
+    if (!it) {
+        SSS_POSTION_THROW(std::runtime_error, "symbol ", s.m_data,
+                          " not exsist");
+    }
+    boost::apply_visitor(raw_stream_visitor(m_o, m_env), *it);
+}
+}  // namespace varlisp
---
+++ varlisp/raw_stream_visitor.hpp
@@ -0,0 +1,32 @@
+#ifndef __RAW_STREAM_VISITOR_HPP_1478481280__
+#define __RAW_STREAM_VISITOR_HPP_1478481280__
+
+#include <boost/variant.hpp>
+
+#include "object.hpp"
+#include "util.hpp"
+
+namespace varlisp {
+
+struct raw_stream_visitor : public boost::static_visitor<void> {
+    // 这个只是负责打印，不应该执行！
+    // 但是，需要获取变量的值；所以：
+    std::ostream& m_o;
+    varlisp::Environment& m_env;
+    raw_stream_visitor(std::ostream& o, varlisp::Environment& env) : m_o(o), m_env(env) {}
+    template <typename T>
+    void operator()(const T& v) const
+    {
+        m_o << v;
+    }
+
+    void operator()(const Empty&) const {}
+    void operator()(bool v) const { m_o << (v ? "true" : "false"); }
+    void operator()(const std::string& v) const { m_o << v; }
+    void operator()(const varlisp::symbol& s) const;
+    void operator()(const sss::regex::CRegex& reg) const { m_o << reg.regstr(); }
+};
+
+}  // namespace varlisp
+
+#endif /* __RAW_STREAM_VISITOR_HPP_1478481280__ */
