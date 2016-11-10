066738a2d373405e92e93a074072ed539dd864c0
sarrow104 <sarrow104@gmail.com>
2016-11-10 10:09:30 +0800

    add getTypedValue(), is_true()

--- varlisp/builtin_helper.hpp
+++ varlisp/builtin_helper.hpp
@@ -1,8 +1,26 @@
 #include "object.hpp"
 
+#include "eval_visitor.hpp"
+
 namespace varlisp {
 struct List;
 struct Environment;
+
+template <typename T>
+inline const T* getTypedValue(varlisp::Environment& env, const varlisp::Object& value,
+                       Object& obj)
+{
+    obj = boost::apply_visitor(eval_visitor(env), value);
+    const T* p_value = boost::get<const T>(&obj);
+    return p_value;
+}
+
+inline bool is_true(varlisp::Environment& env, const varlisp::Object& obj)
+{
+    Object res;
+    const bool * p_bool = getTypedValue<bool>(env, obj, res);
+    return p_bool && *p_bool;
+}
 
 const varlisp::List* getFirstListPtrFromArg(varlisp::Environment& env,
                                             const varlisp::List& args,
