7b55b3e9bf5dad76ee6bb352d78cb49709921453
sarrow104 <sarrow104@gmail.com>
2016-11-10 10:11:56 +0800

    add eval_not(),eval_null(),eval_equal()

--- varlisp/builtin_logic.cpp
+++ varlisp/builtin_logic.cpp
@@ -1,7 +1,10 @@
 #include "object.hpp"
 
+#include "eval_visitor.hpp"
 #include "strict_equal_visitor.hpp"
 #include "strict_less_visitor.hpp"
+
+#include "builtin_helper.hpp"
 
 namespace varlisp {
 // 对于drracket来说，比较运算符，它会直接要求转换到real域；
@@ -92,5 +95,78 @@ Object eval_le(varlisp::Environment& env, const varlisp::List& args)
 
 // TODO (! ) (not ) 当callable，为#f的时候，返回#t；反之，返回#f；
 // 需要注意的是，由于只有#t才判定为真，
+
+/**
+ * @brief (not expr) -> !#t | !#f
+ *
+ * @param[in] env
+ * @param[in] args
+ *
+ * @return
+ */
+Object eval_not(varlisp::Environment& env, const varlisp::List& args)
+{
+    return !varlisp::is_true(env, args.head);
+}
+
+/**
+ * @brief
+ *    (null nil) -> #t
+ *    (null (not nil)) -> #f
+ *
+ * @param[in] env
+ * @param[in] args
+ *
+ * @return
+ */
+Object eval_null(varlisp::Environment& env, const varlisp::List& args)
+{
+    Object obj = boost::apply_visitor(eval_visitor(env), args.head);
+    return !obj.which();
+}
+
+/**
+ * @brief
+ *    (equal '(list1) '(list2)) -> #t | #f
+ *
+ * @param[in] env
+ * @param[in] args
+ *
+ * @return
+ */
+Object eval_equal(varlisp::Environment& env, const varlisp::List& args)
+{
+    Object obj1;
+    const varlisp::List * p_list1 = varlisp::getFirstListPtrFromArg(env, args, obj1);
+    if (!p_list1) {
+        SSS_POSTION_THROW(std::runtime_error,
+                          "(equal: 1st argument must be an s-list)");
+    }
+    Object obj2;
+    const varlisp::List * p_list2 = varlisp::getFirstListPtrFromArg(env, args.tail[0], obj2);
+    if (!p_list2) {
+        SSS_POSTION_THROW(std::runtime_error,
+                          "(equal: 1st argument must be an s-list)");
+    }
+    p_list1 = p_list1->next();
+    p_list2 = p_list2->next();
+    bool is_equal = true;
+    if (p_list1->length() == p_list2->length()) {
+        while (p_list2 && p_list2) {
+            if (!boost::apply_visitor(strict_equal_visitor(env),
+                                    p_list1->head,
+                                    p_list2->head)) {
+                is_equal = false;
+                break;
+            }
+            p_list1 = p_list1->next();
+            p_list2 = p_list2->next();
+        }
+    }
+    else {
+        is_equal = false;
+    }
+    return is_equal;
+}
 
 }  // namespace varlisp
