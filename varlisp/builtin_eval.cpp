cf771c187c3617c900f04aa175714d3e70aeb6ac
sarrow104 <sarrow104@gmail.com>
2016-11-10 10:07:11 +0800

    bugfixed: builtin-eval only support s-list now

--- varlisp/builtin_eval.cpp
+++ varlisp/builtin_eval.cpp
@@ -1,26 +1,28 @@
 #include "eval_visitor.hpp"
 #include "object.hpp"
 
+#include "builtin_helper.hpp"
+
 namespace varlisp {
+/**
+ * @brief (eval '(...)) -> ...
+ *
+ * NOTE DrRacket中 (eval ...) 支持 1 到 2 个参数。
+ *
+ * @param[in] env
+ * @param[in] args
+ *
+ * @return 
+ */
 Object eval_eval(varlisp::Environment& env, const varlisp::List& args)
 {
-    int arg_length = args.length();
-    if (arg_length == 2) {
+    Object obj;
+    const varlisp::List* p_list = getFirstListPtrFromArg(env, args, obj);
+    if (!p_list) {
         SSS_POSTION_THROW(std::runtime_error,
-                          "eval only support one arguments now!");
-        // TODO
-        // 至于环境变量，可以根据传入进来的symbol，然后通过全局函数，获取环
-        // 境变量的引用，然后应用到这里。
+                          "(eval: need squote-List as 1st argument)");
     }
-    // 需要先对参数eval一次，然后再eval一次！
-    // 为什么需要两次？
-    // 因为，既然是通过eval调用进来的，那么，这个eval，根据lisp的语法，必须
-    // 先解析()内部，然后作为外部的参数再次解析。
-    //
-    // 所以，这两次eval动作，分别是原有的用户动作，和这个eval关键字自己引发的动作；
-    varlisp::Object first_res =
-        boost::apply_visitor(eval_visitor(env), args.head);
-    return boost::apply_visitor(eval_visitor(env), first_res);
+    return p_list->next()->eval(env);
 }
 
 }  // namespace varlisp
