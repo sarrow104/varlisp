d5f84b9c45d1c1fd32a740687597aefd9829565c
sarrow104 <sarrow104@gmail.com>
2016-11-10 10:22:11 +0800

    clang-format and sss-colog

--- main.cpp
+++ main.cpp
@@ -5,25 +5,26 @@
 
 #include <linenoise.hpp>
 
+#include <sss/algorithm.hpp>
+#include <sss/colorlog.hpp>
+#include <sss/log.hpp>
 #include <sss/path.hpp>
 #include <sss/utlstring.hpp>
-#include <sss/algorithm.hpp>
-#include <sss/log.hpp>
-#include <sss/colorlog.hpp>
 
 #include "varlisp/interpreter.hpp"
 #include "varlisp/tokenizer.hpp"
 
-const char * find_identifier(const char *buf)
+const char* find_identifier(const char* buf)
 {
     if (!buf || !buf[0]) {
         return 0;
     }
-    const char * ret = std::strchr(buf, '\0');
-    if (std::isspace(*(ret-1))) {
+    const char* ret = std::strchr(buf, '\0');
+    if (std::isspace(*(ret - 1))) {
         return 0;
     }
-    while (std::distance(buf, ret - 1) >= 0 && (std::isalnum(*(ret - 1)) || *(ret - 1) == '-')) {
+    while (std::distance(buf, ret - 1) >= 0 &&
+           (std::isalnum(*(ret - 1)) || *(ret - 1) == '-')) {
         ret--;
     }
     return ret;
@@ -33,7 +34,7 @@ int get_indent(const std::string& line)
 {
     int indent = 0;
     while (indent < int(line.length()) && std::isblank(line[indent])) {
-        indent ++;
+        indent++;
     }
     return indent;
 }
@@ -46,16 +47,20 @@ int test_construct()
     // std::string scripts = "(define a (lambda (x) (* x 2)))";
     varlisp::Interpreter interpreter;
     // interpreter.eval("(define i 0)");
-    interpreter.eval("(define (sqrt-iter guess x) (if (good-enough guess x) guess (sqrt-iter (improve guess x) x)))");
+    interpreter.eval(
+        "(define (sqrt-iter guess x) (if (good-enough guess x) guess "
+        "(sqrt-iter (improve guess x) x)))");
     interpreter.eval("(define (improve guess x) (average guess (/ x guess)))");
     interpreter.eval("(define (average x y) (/ (+ x y) 2))");
-    interpreter.eval("(define (good-enough guess x) (< (abs (- (square guess) x)) 0.001))");
+    interpreter.eval(
+        "(define (good-enough guess x) (< (abs (- (square guess) x)) 0.001))");
     interpreter.eval("(define (sqrt x) (sqrt-iter 1.0 x))");
     interpreter.eval("(define (square x) (* x x))");
     interpreter.eval("(define (abs x) (if (< x 0) (- x) x))");
     interpreter.eval("(sqrt 9)");
 
-    // interpreter.eval("(define fibonacci (lambda (n) (define iter (lambda (i n1 n2) (if (= i 0) n2 (iter (- i 1) n2 (+ n1 n2))))) (iter n 0 1)))");
+    // interpreter.eval("(define fibonacci (lambda (n) (define iter (lambda (i
+    // n1 n2) (if (= i 0) n2 (iter (- i 1) n2 (+ n1 n2))))) (iter n 0 1)))");
     // interpreter.eval("(fibonacci 10)");
     // interpreter.eval("(define x 3)");
     // interpreter.eval("(- x 1)");
@@ -81,18 +86,20 @@ int Interpret()
     // NOTE linenoise默认是单行模式；此时，大段的输入，会导致响应变慢。
     // 因此，修改为多行模式
     linenoise::SetMultiLine(true);
-    linenoise::SetCompletionCallback([&interpreter](const char* editBuffer,
-                                                    std::vector<std::string>& completions) {
+    linenoise::SetCompletionCallback([&interpreter](
+        const char* editBuffer, std::vector<std::string>& completions) {
 
-        const char * last_identifier = find_identifier(editBuffer);
+        const char* last_identifier = find_identifier(editBuffer);
         if (!last_identifier) {
             int width = strlen(editBuffer);
             int tabsize = 8;
             // width = width - width % tabsize + tabsize;
-            completions.push_back(editBuffer + std::string(tabsize - width % tabsize, ' '));
+            completions.push_back(editBuffer +
+                                  std::string(tabsize - width % tabsize, ' '));
             return;
         }
-        std::string prefix = std::string(editBuffer, std::distance(editBuffer, last_identifier));
+        std::string prefix =
+            std::string(editBuffer, std::distance(editBuffer, last_identifier));
         interpreter.retrieve_symbols(completions, last_identifier);
         for (auto& symbol : completions) {
             symbol = prefix + symbol + " ";
@@ -116,41 +123,48 @@ int Interpret()
     std::string last_command;
     int indent = 0;
 
-    while (st != varlisp::Interpreter::status_ERROR && st != varlisp::Interpreter::status_QUIT) {
+    while (st != varlisp::Interpreter::status_ERROR &&
+           st != varlisp::Interpreter::status_QUIT) {
         // NOTE 缩进保持功能丢失了。
-        // 之前，我修改了，install 到 ~/extra/linenoise/ 下的 linenoise.hpp ，以支持 indent风格。
-        // 但是，我在 pull upstream，对linenoise的更新之后，忘记我对哪里，做过修改……
+        // 之前，我修改了，install 到 ~/extra/linenoise/ 下的 linenoise.hpp
+        // ，以支持 indent风格。
+        // 但是，我在 pull
+        // upstream，对linenoise的更新之后，忘记我对哪里，做过修改……
         // 不过，好消息是，新的linenoise，对于tab补全遗留问题，做了修改。
-        auto line = linenoise::Readline(st == varlisp::Interpreter::status_UNFINISHED ? ": " : "> ", indent);
+        auto line = linenoise::Readline(
+            st == varlisp::Interpreter::status_UNFINISHED ? ": " : "> ",
+            indent);
         indent = get_indent(line);
 
         switch (st) {
-        case varlisp::Interpreter::status_UNFINISHED:
-            st = interpreter.eval(line);
-            break;
-
-        case varlisp::Interpreter::status_OK:
-            if (sss::is_all_blank(line)) {
-                continue;
-            }
-            else {
+            case varlisp::Interpreter::status_UNFINISHED:
                 st = interpreter.eval(line);
-            }
-            break;
+                break;
 
-        case varlisp::Interpreter::status_ERROR:
+            case varlisp::Interpreter::status_OK:
+                if (sss::is_all_blank(line)) {
+                    continue;
+                }
+                else {
+                    st = interpreter.eval(line);
+                }
+                break;
 
-        default:
-            break;
+            case varlisp::Interpreter::status_ERROR:
+
+            default:
+                break;
         }
 
-        if (st == varlisp::Interpreter::status_OK || st == varlisp::Interpreter::status_UNFINISHED) {
+        if (st == varlisp::Interpreter::status_OK ||
+            st == varlisp::Interpreter::status_UNFINISHED) {
             if (!last_command.empty()) {
                 sss::rtrim(last_command);
                 last_command += " ";
             }
             // NOTE
-            // 在 status_UNFINISHED 状态下，清除左侧的空白，是不安全的作法——不过关系不大；
+            // 在 status_UNFINISHED
+            // 状态下，清除左侧的空白，是不安全的作法——不过关系不大；
             // 我的token，可不是按行处理的。
             // 对于当前的token模式，rtrim是安全的
             sss::ltrim(line);
@@ -176,12 +190,15 @@ int Interpret()
     return EXIT_SUCCESS;
 }
 
-int main (int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    (void) argc;
-    (void) argv;
+    (void)argc;
+    (void)argv;
 
-    sss::colog::set_log_elements(sss::colog::ls_TIME_NANO);
+    sss::colog::set_log_elements(sss::colog::ls_TIME_NANO |
+                                 sss::colog::ls_FILE | sss::colog::ls_LINE);
+    sss::colog::set_log_levels(sss::colog::ll_INFO | sss::colog::ll_ERROR |
+                               sss::colog::ll_WARN | sss::colog::ll_FATAL);
 #if 1
     return Interpret();
 #else
@@ -195,5 +212,6 @@ int main (int argc, char *argv[])
 // (define pi 3.14159)
 // (* pi (* radius radius))
 // (define circumference (* 2 pi radius))
-// (quit) 
-// (define fibonacci (lambda (n) (define iter (lambda (i n1 n2) (if (= i 0) n2 (iter (- i 1) n2 (+ n1 n2))))) (iter n 0 1)))
+// (quit)
+// (define fibonacci (lambda (n) (define iter (lambda (i n1 n2) (if (= i 0) n2
+// (iter (- i 1) n2 (+ n1 n2))))) (iter n 0 1)))
