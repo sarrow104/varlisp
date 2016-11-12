e5d4ecfd0d734267c2018e7dc3fcbeab13814bf9
sarrow104 <sarrow104@gmail.com>
2016-11-12 14:33:33 +0800

    add builtin-time

---
+++ varlisp/builtin_time.cpp
@@ -0,0 +1,29 @@
+#include "object.hpp"
+
+#include "builtin_helper.hpp"
+#include <sss/colorlog.hpp>
+
+#include <sstream>
+
+namespace varlisp {
+
+/**
+ * @brief
+ *      (time expr) -> result
+ *
+ * @param[in] env
+ * @param[in] args
+ *
+ * @return
+ */
+Object eval_time(varlisp::Environment &env, const varlisp::List &args)
+{
+    std::ostringstream exprstr;
+    exprstr << args.head;
+    COLOG_INFO("start eval ", exprstr.str());
+    Object res;
+    const Object& res_ref = getAtomicValue(env, args.head, res);
+    COLOG_INFO("end eval ", exprstr.str());
+    return res_ref;
+}
+} // namespace varlisp
