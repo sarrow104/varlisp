24d599d45cc8ea5c764972910f12457fc7fadcfd
sarrow104 <sarrow104@gmail.com>
2016-11-12 11:16:59 +0800

    refcactory: Nill support; adjust operator << Object position

--- varlisp/object.hpp
+++ varlisp/object.hpp
@@ -18,6 +18,14 @@ struct Empty {
 inline std::ostream& operator<<(std::ostream& o, const Empty&) { return o; }
 inline bool operator==(const Empty&, const Empty&) { return true; }
 inline bool operator<(const Empty&, const Empty&) { return false; }
+
+struct Nill {
+};
+
+inline std::ostream& operator<<(std::ostream& o, const Nill&) { return o; }
+inline bool operator==(const Nill&, const Nill&) { return true; }
+inline bool operator<(const Nill&, const Nill&) { return false; }
+
 struct Define;
 struct IfExpr;
 struct List;
@@ -28,25 +36,27 @@ struct Cond;
 struct LogicAnd;
 struct LogicOr;
 
+// NOTE
 typedef boost::variant<
     Empty,                                  // 0
-    bool,                                   // 1
-    int,                                    // 2
-    double,                                 // 3
-    std::string,                            // 4
-    sss::regex::CRegex,                     // 5
-    symbol,                                 // 6
-    boost::recursive_wrapper<Builtin>,      // 7
-    boost::recursive_wrapper<Define>,       // 8
-    boost::recursive_wrapper<IfExpr>,       // 9
-    boost::recursive_wrapper<Cond>,         // 10
-    boost::recursive_wrapper<LogicAnd>,     // 11
-    boost::recursive_wrapper<LogicOr>,      // 12
-    boost::recursive_wrapper<List>,         // 13
+    Nill,                                   // 1
+    bool,                                   // 2
+    int,                                    // 3
+    double,                                 // 4
+    std::string,                            // 5
+    sss::regex::CRegex,                     // 6
+    symbol,                                 // 7
+    boost::recursive_wrapper<Builtin>,      // 8
+    boost::recursive_wrapper<Define>,       // 9
+    boost::recursive_wrapper<IfExpr>,       // 10
+    boost::recursive_wrapper<Cond>,         // 11
+    boost::recursive_wrapper<LogicAnd>,     // 12
+    boost::recursive_wrapper<LogicOr>,      // 13
+    boost::recursive_wrapper<List>,         // 14
     // NOTE
     // quote-list只是作为一种函数存在！
-    boost::recursive_wrapper<Lambda>,       // 14
-    gumboNode
+    boost::recursive_wrapper<Lambda>,       // 15
+    gumboNode                               // 16
     >
     Object;
 }  // namespace varlisp
@@ -60,6 +70,12 @@ typedef boost::variant<
 #include "logic_and.hpp"
 #include "logic_or.hpp"
 
-std::ostream& operator<<(std::ostream& o, const varlisp::Object& obj);
+#include "print_visitor.hpp"
+
+inline std::ostream& operator<<(std::ostream& o, const varlisp::Object& obj)
+{
+    boost::apply_visitor(varlisp::print_visitor(o), obj);
+    return o;
+}
 
 #endif /* __OBJECT_HPP_1457602801__ */
