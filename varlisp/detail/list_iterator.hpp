9651a651da42cfa56d14456c4f52eaf246f9eab0
sarrow104 <sarrow104@gmail.com>
2016-12-01 07:20:29 +0800

    simpler list_back_inserter

--- varlisp/detail/list_iterator.hpp
+++ varlisp/detail/list_iterator.hpp
@@ -1,17 +1,18 @@
 #pragma once
 
+#include <stdexcept>
+
+#include <sss/util/PostionThrow.hpp>
+
 #include "../object.hpp"
 
 namespace varlisp {
 
 namespace detail {
-template<typename Out>
-struct Converter
-{
-    Converter(Object& obj) : m_obj(obj)
-    {
-    }
-    Converter& operator = (const Out& v)
+template <typename Out>
+struct Converter {
+    Converter(Object& obj) : m_obj(obj) {}
+    Converter& operator=(const Out& v)
     {
         m_obj = v;
         COLOG_DEBUG(v);
@@ -20,9 +21,8 @@ struct Converter
 
     Object& m_obj;
 };
-template<typename Out = varlisp::Object>
-struct list_back_inserter_t
-{
+template <typename Out = varlisp::Object>
+struct list_back_inserter_t {
     explicit list_back_inserter_t(varlisp::List& list)
         : m_list(list), m_list_ptr(nullptr)
     {
@@ -38,34 +38,39 @@ struct list_back_inserter_t
         COLOG_DEBUG(m_list_ptr, m_list_ptr->head.which(), m_list_ptr->head);
     }
     ~list_back_inserter_t() = default;
-    list_back_inserter_t operator++(int)  {
+    list_back_inserter_t operator++(int)
+    {
         list_back_inserter_t ret(*this);
         this->next();
         COLOG_DEBUG(ret.m_list_ptr, m_list_ptr);
         return ret;
     }
-    list_back_inserter_t& operator++()  {
-        varlisp::List * p_l = m_list_ptr;
+    list_back_inserter_t& operator++()
+    {
+        varlisp::List* p_l = m_list_ptr;
         this->next();
         COLOG_DEBUG(p_l, m_list_ptr);
         return *this;
     }
-    void next() {
-        m_list_ptr = m_list_ptr->next_slot();
-    }
-    varlisp::List & m_list;
-    varlisp::List * m_list_ptr;
-    Out operator*() {
+    void next() { m_list_ptr = m_list_ptr->next_slot(); }
+    varlisp::List& m_list;
+    varlisp::List* m_list_ptr;
+    Out operator*()
+    {
         COLOG_DEBUG(m_list_ptr, m_list_ptr->head.which());
         return m_list_ptr->head;
     }
 };
 
-template<typename T>
-struct list_const_iterator_t
-{
-    explicit list_const_iterator_t(const varlisp::List * p_list)
+template <typename T>
+struct list_const_iterator_t {
+    explicit list_const_iterator_t(const varlisp::List* p_list)
         : m_list_ptr(p_list)
+    {
+        COLOG_DEBUG(m_list_ptr);
+    }
+    list_const_iterator_t()
+        : m_list_ptr(nullptr)
     {
         COLOG_DEBUG(m_list_ptr);
     }
@@ -74,46 +79,54 @@ struct list_const_iterator_t
     {
         COLOG_DEBUG(m_list_ptr);
     }
-    bool operator != (const list_const_iterator_t& ref) const {
+    bool operator!=(const list_const_iterator_t& ref) const
+    {
         return m_list_ptr != ref.m_list_ptr;
     }
-    bool operator == (const list_const_iterator_t& ref) const {
+    bool operator==(const list_const_iterator_t& ref) const
+    {
         return m_list_ptr == ref.m_list_ptr;
     }
-    const T& operator* () const {
+    const T& operator*() const
+    {
         if (!m_list_ptr) {
             SSS_POSITION_THROW(std::runtime_error, "nullptr");
         }
-        const T * p_val = boost::get<T>(&m_list_ptr->head);
+        const T* p_val = boost::get<T>(&m_list_ptr->head);
         if (!p_val) {
             SSS_POSITION_THROW(std::runtime_error, m_list_ptr->head.which(),
                                m_list_ptr->head);
         }
-        return  *p_val;
+        return *p_val;
     }
-    list_const_iterator_t operator++(int)  {
+    list_const_iterator_t operator++(int)
+    {
         list_const_iterator_t ret(*this);
         this->next();
         return ret;
     }
-    list_const_iterator_t& operator++()  {
+    list_const_iterator_t& operator++()
+    {
         this->next();
         return *this;
     }
-    void next() {
+    void next()
+    {
         m_list_ptr = m_list_ptr->next();
         if (!m_list_ptr->head.which()) {
             m_list_ptr = 0;
         }
     }
-    const varlisp::List * m_list_ptr;
+    const varlisp::List* m_list_ptr;
 };
-template<typename Out>
-list_back_inserter_t<Out> list_back_inserter(varlisp::List& list)
+template <typename Out>
+list_back_inserter_t<Converter<Out>> list_back_inserter(varlisp::List& list)
 {
-    return list_back_inserter_t<Out>(list);
+    return list_back_inserter_t<Converter<Out>>(list);
 }
 
-} // namespace detail
+// typedef list_back_inserter_t<Converter<Object> > list_object_back_inserter;
 
-} // namespace varlisp
+}  // namespace detail
+
+}  // namespace varlisp
