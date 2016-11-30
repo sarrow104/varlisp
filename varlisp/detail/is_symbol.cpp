dea26ebb95bf6bc41525f1880d918aa570453568
sarrow104 <sarrow104@gmail.com>
2016-11-30 21:05:54 +0800

    add detail::is_symbol function

---
+++ varlisp/detail/is_symbol.cpp
@@ -0,0 +1,29 @@
+#include "is_symbol.hpp"
+
+#include "../keyword_t.hpp"
+
+namespace varlisp {
+namespace detail {
+
+bool is_symbol(const std::string& name)
+{
+    varlisp::Tokenizer t{name};
+    auto tok = t.top();
+    if (t.lookahead_nothrow(1).which()) {
+        return false;
+    }
+    varlisp::symbol * p_sym = boost::get<varlisp::symbol>(&tok);
+    if (!p_sym) {
+        return false;
+    }
+    if (p_sym->m_data != name) {
+        return false;
+    }
+    if (varlisp::keywords_t::is_keyword(p_sym->m_data)) {
+        return false;
+    }
+    return true;
+}
+} // namespace detail
+} // namespace varlisp
+
