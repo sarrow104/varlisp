929e4a5ba17c6154881e35e199ea52a65bfea786
sarrow104 <sarrow104@gmail.com>
2016-12-01 14:45:02 +0800

    add empty help-doc err-msg print

--- varlisp/helpmsg_visitor.cpp
+++ varlisp/helpmsg_visitor.cpp
@@ -1,6 +1,7 @@
 #include "helpmsg_visitor.hpp"
 
 #include <sss/path.hpp>
+#include <sss/colorlog.hpp>
 
 #include "object.hpp"
 #include "builtin_helper.hpp"
@@ -13,9 +14,10 @@ namespace varlisp {
 
 varlisp::string_t helpmsg_visitor::operator()(const varlisp::symbol& s) const
 {
+    varlisp::string_t help_msg;
     const char * keyword_help_msg = varlisp::keywords_t::help_msg(s.m_data);
     if (keyword_help_msg[0]) {
-        return varlisp::string_t(keyword_help_msg);
+        help_msg = varlisp::string_t(keyword_help_msg);
     }
     else {
         Object* it = m_env.find(s.m_data);
@@ -26,14 +28,18 @@ varlisp::string_t helpmsg_visitor::operator()(const varlisp::symbol& s) const
         Object obj;
         const Object& objref = varlisp::getAtomicValue(m_env, *it, obj);
         if (const varlisp::Builtin * p_f = boost::get<varlisp::Builtin>(&objref)) {
-            return varlisp::detail::get_builtin_infos()[p_f->type()].help_msg;
+            help_msg = varlisp::detail::get_builtin_infos()[p_f->type()].help_msg;
         }
         else if (const varlisp::Lambda * p_tmp = boost::get<varlisp::Lambda>(&objref)) {
-            return p_tmp->help_doc;
+            help_msg = p_tmp->help_doc;
         }
         else {
-            return boost::apply_visitor(helpmsg_visitor(m_env), objref);
+            help_msg = boost::apply_visitor(helpmsg_visitor(m_env), objref);
         }
     }
+    if (help_msg.empty()) {
+        COLOG_ERROR("there is no help-msg for `", s, "`");
+    }
+    return help_msg;
 }
 } // namespace varlisp
