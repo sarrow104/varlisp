84ed371f8eeedbb6a481afa6d2a5348abe3c694c
sarrow104 <sarrow104@gmail.com>
2016-12-06 21:57:09 +0800

    add keywords_t

--- varlisp/helpmsg_visitor.cpp
+++ varlisp/helpmsg_visitor.cpp
@@ -15,31 +15,31 @@ namespace varlisp {
 varlisp::string_t helpmsg_visitor::operator()(const varlisp::symbol& s) const
 {
     varlisp::string_t help_msg;
-    const char * keyword_help_msg = varlisp::keywords_t::help_msg(s.m_data);
-    if (keyword_help_msg[0]) {
-        help_msg = varlisp::string_t(keyword_help_msg);
+    Object* it = m_env.find(s.m_data);
+    if (!it) {
+        SSS_POSITION_THROW(std::runtime_error, "symbol ", s.m_data,
+                           " not exsist");
+    }
+    Object obj;
+    const Object& objref = varlisp::getAtomicValue(m_env, *it, obj);
+    if (const varlisp::Builtin * p_f = boost::get<varlisp::Builtin>(&objref)) {
+        help_msg = varlisp::detail::get_builtin_infos()[p_f->type()].help_msg;
+    }
+    else if (const varlisp::Lambda * p_tmp = boost::get<varlisp::Lambda>(&objref)) {
+        help_msg = p_tmp->help_doc;
     }
     else {
-        Object* it = m_env.find(s.m_data);
-        if (!it) {
-            SSS_POSITION_THROW(std::runtime_error, "symbol ", s.m_data,
-                               " not exsist");
-        }
-        Object obj;
-        const Object& objref = varlisp::getAtomicValue(m_env, *it, obj);
-        if (const varlisp::Builtin * p_f = boost::get<varlisp::Builtin>(&objref)) {
-            help_msg = varlisp::detail::get_builtin_infos()[p_f->type()].help_msg;
-        }
-        else if (const varlisp::Lambda * p_tmp = boost::get<varlisp::Lambda>(&objref)) {
-            help_msg = p_tmp->help_doc;
-        }
-        else {
-            help_msg = boost::apply_visitor(helpmsg_visitor(m_env), objref);
-        }
+        help_msg = boost::apply_visitor(helpmsg_visitor(m_env), objref);
     }
     if (help_msg.empty()) {
         COLOG_ERROR("there is no help-msg for `", s, "`");
     }
     return help_msg;
 }
+
+varlisp::string_t helpmsg_visitor::operator()(const varlisp::keywords_t& k ) const
+{
+    return varlisp::string_t(k.help_msg(), true);
+}
+
 } // namespace varlisp
--- varlisp/helpmsg_visitor.hpp
+++ varlisp/helpmsg_visitor.hpp
@@ -13,6 +13,7 @@ struct Empty;
 struct Nill;
 struct symbol;
 struct String;
+struct keywords_t;
 typedef String string_t;
 struct Environment;
 
@@ -28,11 +29,12 @@ struct helpmsg_visitor : public boost::static_visitor<varlisp::string_t> {
     varlisp::string_t operator()(const Empty&) const {
         return varlisp::string_t{"empty"};
     }
-    varlisp::string_t operator()(const Nill&) const { return varlisp::string_t{"nil"}; }
-    varlisp::string_t operator()(bool v) const { return varlisp::string_t{v ? "#t" : "#f"}; }
-    varlisp::string_t operator()(const string_t& ) const { return varlisp::string_t{"string"}; }
-    varlisp::string_t operator()(const varlisp::symbol& s) const;
-    varlisp::string_t operator()(const sss::regex::CRegex& ) const { return varlisp::string_t{"regex"}; }
+    varlisp::string_t operator()(const Nill&               ) const { return varlisp::string_t{"nil"};           }
+    varlisp::string_t operator()(bool v                    ) const { return varlisp::string_t{v ? "#t" : "#f"}; }
+    varlisp::string_t operator()(const string_t&           ) const { return varlisp::string_t{"string"};        }
+    varlisp::string_t operator()(const varlisp::symbol& s  ) const ;
+    varlisp::string_t operator()(const varlisp::keywords_t& k ) const ;
+    varlisp::string_t operator()(const sss::regex::CRegex& ) const { return varlisp::string_t{"regex"};         }
 };
 
 }  // namespace varlisp
