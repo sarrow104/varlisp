24f87bc3a75599e998f8b792bd318811b229da2c
sarrow104 <sarrow104@gmail.com>
2016-12-06 17:12:59 +0800

    complement keywords_t member

--- varlisp/keyword_t.cpp
+++ varlisp/keyword_t.cpp
@@ -1,5 +1,11 @@
 #include "keyword_t.hpp"
 
+#include <stdexcept>
+
+#include <sss/util/PostionThrow.hpp>
+
+#include "String.hpp"
+
 namespace varlisp {
 bool keywords_t::is_keyword(const std::string& name)
 {
@@ -7,7 +13,7 @@ bool keywords_t::is_keyword(const std::string& name)
 }
 const char * keywords_t::help_msg(const std::string& name)
 {
-    switch (varlisp::detail::keywords_hash::hash(name)) {
+    switch (keywords_t::gen_hash(name)) {
         case kw_NONE:
             return "";
             break;
@@ -63,8 +69,140 @@ const char * keywords_t::help_msg(const std::string& name)
             return "keywords <list>\n\t(list ...)";
             break;
 
+        case kw_QUOTE:
+            return "kwyeords <quote>\n\t(quote ...)\n\t'...";
+
+        case kw_CONTEXT:
+            return "kwyeords <quote>\n\t(context ...)\n\t{...}";
+
         default:
+            SSS_POSITION_THROW(std::runtime_error,
+                               name);
             return "";
     }
 }
+
+keywords_t::kw_type_t keywords_t::gen_hash(const std::string& name)
+{
+    uint64_t hash =
+        varlisp::detail::keywords_hash::hash(name);
+    switch (hash) {
+        case kw_IF:
+            return kw_IF;
+
+        case kw_ELSE:
+            return kw_ELSE;
+
+        case kw_DEFINE:
+            return kw_DEFINE;
+
+        case kw_COND:
+            return kw_COND;
+            // 另外一种写法：
+            // (COND
+            // 	(condition1   result1 )
+            // 	(condition2   result2 )
+            // 	...
+            // 	(#t    resultN ) ) ; #t这里，是保证有值可以返回
+            //! http://www.cis.upenn.edu/~matuszek/LispText/lisp-cond.html
+
+        case kw_AND:
+            return kw_AND;
+
+        case kw_OR:
+            return kw_OR;
+
+        case kw_LAMBDA:
+            return kw_LAMBDA;
+
+        case kw_NIL:
+            return kw_NIL;
+
+        case kw_LIST:
+            return kw_LIST;
+
+        case kw_QUOTE:
+            return kw_QUOTE;
+
+        case kw_CONTEXT:
+            return kw_CONTEXT;
+
+        default:
+            return kw_NONE;
+    }
+}
+
+const string_t& keywords_t::gen_name(keywords_t::kw_type_t t)
+{
+    static string_t name_none{""};
+#ifndef MAKE_NAME
+#define MAKE_NAME(a) static string_t name_##a{#a};
+#endif
+
+    MAKE_NAME(if);
+    MAKE_NAME(else);
+    MAKE_NAME(define);
+    MAKE_NAME(cond);
+    MAKE_NAME(and);
+    MAKE_NAME(or);
+    MAKE_NAME(lambda);
+    MAKE_NAME(nil);
+    MAKE_NAME(list);
+    MAKE_NAME(quote);
+    MAKE_NAME(context);
+
+#undef MAKE_NAME
+
+    switch (t) {
+        case kw_NONE:
+            return name_none;
+
+        case kw_IF:
+            return name_if;
+
+        case kw_ELSE:
+            return name_else;
+
+        case kw_DEFINE:
+            return name_define;
+
+        case kw_COND:
+            return name_cond;
+
+        case kw_AND:
+            return name_and;
+
+        case kw_OR:
+            return name_or;
+
+        case kw_LAMBDA:
+            return name_lambda;
+
+        case kw_NIL:
+            return name_nil;
+
+        case kw_LIST:
+            return name_list;
+
+        case kw_QUOTE:
+            return name_quote;
+
+        case kw_CONTEXT:
+            return name_context;
+
+        default:
+            SSS_POSITION_THROW(std::runtime_error,
+                               t);
+    }
+}
+
+sss::string_view keywords_t::name() const {
+    return keywords_t::gen_name(m_hash_value).to_string_view();
+}
+
+void keywords_t::print(std::ostream& o) const
+{
+    o << keywords_t::gen_name(this->m_hash_value);
+}
+
 } // namespace varlisp
--- varlisp/keyword_t.hpp
+++ varlisp/keyword_t.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <iosfwd>
+
+#include <sss/string_view.hpp>
+
 #include "literal_hash.hpp"
 
 namespace varlisp {
+
+struct String;
+typedef String string_t;
 /**
  * @brief 
  *    标识关键字；提供关键字的一些说明和识别功能；
@@ -11,19 +18,77 @@ struct keywords_t
 {
     enum kw_type_t {
         // using namespace varlisp::detail::keywords_hash;
-        kw_NONE   = 0,
-        kw_IF     = varlisp::detail::keywords_hash::hash("if"),
-        kw_ELSE   = varlisp::detail::keywords_hash::hash("else"),
-        kw_DEFINE = varlisp::detail::keywords_hash::hash("define"),
-        kw_COND   = varlisp::detail::keywords_hash::hash("cond"),
-        kw_AND    = varlisp::detail::keywords_hash::hash("and"),
-        kw_OR     = varlisp::detail::keywords_hash::hash("or"),
-        kw_LAMBDA = varlisp::detail::keywords_hash::hash("lambda"),
-        kw_NIL    = varlisp::detail::keywords_hash::hash("nil"),
-        kw_LIST   = varlisp::detail::keywords_hash::hash("list")
+        kw_NONE    = 0,
+        kw_IF      = varlisp::detail::keywords_hash::hash("if"),
+        kw_ELSE    = varlisp::detail::keywords_hash::hash("else"),
+        kw_DEFINE  = varlisp::detail::keywords_hash::hash("define"),
+        kw_COND    = varlisp::detail::keywords_hash::hash("cond"),
+        kw_AND     = varlisp::detail::keywords_hash::hash("and"),
+        kw_OR      = varlisp::detail::keywords_hash::hash("or"),
+        kw_LAMBDA  = varlisp::detail::keywords_hash::hash("lambda"),
+        kw_NIL     = varlisp::detail::keywords_hash::hash("nil"),
+        kw_LIST    = varlisp::detail::keywords_hash::hash("list"),  // list 和 quote 等效
+        kw_QUOTE   = varlisp::detail::keywords_hash::hash("quote"),
+        kw_CONTEXT = varlisp::detail::keywords_hash::hash("context"),
     };
     static bool is_keyword(const std::string& name);
     static const char * help_msg(const std::string& name);
+
+    static kw_type_t gen_hash(const std::string& name);
+    static const string_t& gen_name(kw_type_t t);
+    
+    keywords_t(kw_type_t t)
+        : m_hash_value(t)
+    {
+    }
+    explicit keywords_t(const char * name)
+    {
+        succeed_or_throw(name);
+    }
+    template<size_t N>
+    explicit keywords_t(const char (&name)[N])
+    {
+        succeed_or_throw(name);
+    }
+    ~keywords_t() = default;
+
+    explicit keywords_t(const std::string& name)
+    {
+        succeed_or_throw(name);
+    }
+
+    void succeed_or_throw(const std::string& name)
+    {
+        if (!keywords_t::is_keyword(name)) {
+            throw name;
+        }
+        m_hash_value = (kw_type_t)varlisp::detail::keywords_hash::hash(name);
+    }
+
+    bool operator ==(const keywords_t& rhs) const {
+        return m_hash_value == rhs.m_hash_value;
+    }
+
+    bool operator <(const keywords_t& rhs) const {
+        return m_hash_value < rhs.m_hash_value;
+    }
+
+    sss::string_view name() const;
+
+    void print(std::ostream& o) const;
+
+    kw_type_t type() const {
+        return m_hash_value;
+    }
+
+private:
+    kw_type_t m_hash_value;
 };
+
+inline std::ostream& operator << (std::ostream& o, const keywords_t& key)
+{
+    key.print(o);
+    return o;
+}
 
 } // namespace varlisp
