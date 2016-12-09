ccc8356d901283fa3c0765354efa233e4b625078
sarrow104 <sarrow104@gmail.com>
2016-12-09 15:14:39 +0800

    sync with new-List model; add json-string

--- varlisp/builtin_json.cpp
+++ varlisp/builtin_json.cpp
@@ -8,19 +8,15 @@
 #include "detail/buitin_info_t.hpp"
 #include "json_print_visitor.hpp"
 #include "json/parser.hpp"
+#include "detail/car.hpp"
 
 namespace varlisp {
 
-REGIST_BUILTIN("json-print", 1, 1, eval_json_print,
-               "; json-print 用json格式，打印內建对象；\n"
-               "; 注意，仅支持list和env\n"
-               "(json-print obj) -> nil");
-
-Object eval_json_print(varlisp::Environment& env, const varlisp::List& args)
+namespace detail {
+void json_object2stream(std::ostream& o, varlisp::Environment& env, const Object& obj, const char * funcName)
 {
-    const char * funcName = "json-print";
     Object tmp;
-    const Object& objRef = varlisp::getAtomicValue(env, args.head, tmp);
+    const Object& objRef = varlisp::getAtomicValue(env, obj, tmp);
     if (boost::get<varlisp::Environment>(&objRef)) {
         // OK
     }
@@ -37,8 +33,34 @@ Object eval_json_print(varlisp::Environment& env, const varlisp::List& args)
                            "(", funcName, ": only Environment type or s-list type is valid; but ",
                            objRef, ")");
     }
-    boost::apply_visitor(json_print_visitor(std::cout), objRef);
+    boost::apply_visitor(json_print_visitor(o), objRef);
+}
+
+} // namespace detail
+
+REGIST_BUILTIN("json-print", 1, 1, eval_json_print,
+               "; json-print 用json格式，打印內建对象；\n"
+               "; 注意，仅支持list和env\n"
+               "(json-print obj) -> nil");
+
+Object eval_json_print(varlisp::Environment& env, const varlisp::List& args)
+{
+    const char * funcName = "json-print";
+    detail::json_object2stream(std::cout, env, detail::car(args), funcName);
     return Nill{};
+}
+
+REGIST_BUILTIN("json-string", 1, 1, eval_json_string,
+               "; json-string 用json格式，序列化內建对象；\n"
+               "; 注意，仅支持list和env\n"
+               "(json-string obj) -> nil");
+
+Object eval_json_string(varlisp::Environment& env, const varlisp::List& args)
+{
+    const char * funcName = "json-string";
+    std::ostringstream oss;
+    detail::json_object2stream(oss, env, detail::car(args), funcName);
+    return string_t{std::move(oss.str())};
 }
 
 REGIST_BUILTIN("json-parse", 1, 1, eval_json_parse,
@@ -52,11 +74,11 @@ Object eval_json_parse(varlisp::Environment& env, const varlisp::List& args)
     const char * funcName = "json-parse";
     // 这基本需要重写解析器了
     Object obj;
-    const string_t * p_s = varlisp::getTypedValue<varlisp::string_t>(env, args.head, obj);
+    const string_t * p_s = varlisp::getTypedValue<varlisp::string_t>(env, detail::car(args), obj);
     if (!p_s) {
         SSS_POSITION_THROW(std::runtime_error,
                            "(", funcName, ": 1st argument must be string; but ",
-                           args.head, ")");
+                           detail::car(args), ")");
     }
 
     return json::parse(p_s->to_string_view());
