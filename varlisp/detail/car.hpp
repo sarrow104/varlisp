ef9f9d48a82f888c6fb2f79513250ad9001136d5
sarrow104 <sarrow104@gmail.com>
2016-12-09 10:14:22 +0800

    sync with new-List model

--- varlisp/detail/car.hpp
+++ varlisp/detail/car.hpp
@@ -6,62 +6,42 @@ namespace varlisp {
 namespace detail {
 inline const Object& car(const varlisp::List& args)
 {
-    return args.head;
+    return args.head();
 }
 
 inline Object& car(varlisp::List& args)
 {
-    return args.head;
+    return args.head();
 }
 
 inline const Object& cadr(const varlisp::List& args)
 {
-    return args.tail[0].head;
+    return args.tail().head();
 }
 
 inline Object& cadr(varlisp::List& args)
 {
-    return args.tail[0].head;
+    return args.tail().head();
 }
 
 inline const Object& caddr(const varlisp::List& args)
 {
-    return args.tail[0].tail[0].head;
+    return args.tail().tail().head();
 }
 
 inline Object& caddr(varlisp::List& args)
 {
-    return args.tail[0].tail[0].head;
+    return args.tail().tail().head();
 }
 
-inline bool is_car_valid(const varlisp::List * p_list)
+inline varlisp::List tail(const varlisp::List & args)
 {
-    return p_list && p_list->head.which();
+    return args.tail();
 }
 
-inline bool is_car_valid(const varlisp::List & list)
+inline varlisp::List tail(varlisp::List & args)
 {
-    return list.head.which();
-}
-
-inline const varlisp::List * tail(const varlisp::List & args)
-{
-    if (is_car_valid(args) && !args.tail.empty()) {
-        return &args.tail[0];
-    }
-    else {
-        return nullptr;
-    }
-}
-
-inline varlisp::List * tail(varlisp::List & args)
-{
-    if (is_car_valid(args) && !args.tail.empty()) {
-        return &args.tail[0];
-    }
-    else {
-        return nullptr;
-    }
+    return args.tail();
 }
 
 } // namespace detail
