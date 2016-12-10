b8a39aa12d8e47e951fdd04468071a161d2a2c3a
sarrow104 <sarrow104@gmail.com>
2016-12-10 10:35:04 +0800

    add elapsed time print

--- varlisp/builtin_time.cpp
+++ varlisp/builtin_time.cpp
@@ -1,4 +1,5 @@
 #include <sstream>
+#include <chrono>
 
 #include <sss/colorlog.hpp>
 
@@ -24,11 +25,52 @@ Object eval_time(varlisp::Environment &env, const varlisp::List &args)
 {
     std::ostringstream exprstr;
     exprstr << detail::car(args);
+    std::chrono::high_resolution_clock clock;
+    std::chrono::high_resolution_clock::time_point reset_time;
+    reset_time = clock.now();
     COLOG_INFO("start eval ", exprstr.str());
     Object res;
     const Object& res_ref = getAtomicValue(env, detail::car(args), res);
+
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(clock.now() - reset_time).count();
+
     COLOG_INFO("end eval ", exprstr.str());
+    COLOG_INFO("elapsed = ", elapsed / 1000.0);
     return res_ref;
 }
+
+REGIST_BUILTIN("date", 0, 0, eval_date, "(date) -> [year month day]");
+Object eval_date(varlisp::Environment &env, const varlisp::List &args)
+{
+    // TODO
+    return Nill{};
+}
+
+REGIST_BUILTIN("date-time", 0, 0, eval_date_time, "(date-time) -> [year month day HH MM SS]");
+
+Object eval_date_time(varlisp::Environment &env, const varlisp::List &args)
+{
+    // TODO
+    return Nill{};
+}
+
+REGIST_BUILTIN("date-time-nano", 0, 0, eval_date_time_nano, "(date-time-nano) -> [year month day HH MM SS nano]");
+
+Object eval_date_time_nano(varlisp::Environment &env, const varlisp::List &args)
+{
+    // TODO
+    return Nill{};
+}
+
+// TODO strftime(int64_t)
+// int64_t parse_time(string)
+// > (define l [2016 12 9])
+// nil
+// > l
+// [2016 12 9]
+// > (setq l:0 2017)
+// 2017
+// > l
+// [2017 12 9]
 
 } // namespace varlisp
