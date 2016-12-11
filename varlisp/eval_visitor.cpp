ce24197c70eb61cea8d2cba823dc3243581d61c9
sarrow104 <sarrow104@gmail.com>
2016-12-11 13:15:26 +0800

    Environment::find() split into deep_find() and find()

--- varlisp/eval_visitor.cpp
+++ varlisp/eval_visitor.cpp
@@ -9,7 +9,7 @@ namespace varlisp {
 
 Object eval_visitor::operator()(const varlisp::symbol& s) const
 {
-    Object* it = m_env.find(s.name());
+    Object* it = m_env.deep_find(s.name());
     if (!it) {
         SSS_POSITION_THROW(std::runtime_error, "symbol ", s.name(),
                           " not exsist");
