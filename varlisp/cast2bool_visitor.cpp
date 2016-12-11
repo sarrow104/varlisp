3b4a76b6885c908b6e0ce6ca3f868a7dc9989a3b
sarrow104 <sarrow104@gmail.com>
2016-12-11 13:14:36 +0800

    Environment::find() split into deep_find() and find()

--- varlisp/cast2bool_visitor.cpp
+++ varlisp/cast2bool_visitor.cpp
@@ -14,7 +14,7 @@ bool cast2bool_visitor::operator()(const string_t& s) const
 
 bool cast2bool_visitor::operator()(const varlisp::symbol& s) const
 {
-    Object* it = m_env.find(s.name());
+    Object* it = m_env.deep_find(s.name());
     if (!it) {
         SSS_POSITION_THROW(std::runtime_error, "symbol ", s.name(),
                           " not exists!");
