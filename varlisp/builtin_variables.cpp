c00deb796f1a7c19e2dd366c515c783c4997ff04
sarrow104 <sarrow104@gmail.com>
2016-12-13 16:02:45 +0800

    upgrade (undef); add (letn), refactory (let); add (locate ...),(symbols ...)

--- varlisp/builtin_variables.cpp
+++ varlisp/builtin_variables.cpp
@@ -14,6 +14,8 @@
 #include "tokenizer.hpp"
 #include "keyword_t.hpp"
 #include "detail/car.hpp"
+#include "detail/json_accessor.hpp"
+#include "detail/list_iterator.hpp"
 
 namespace varlisp {
 
@@ -43,18 +45,36 @@ Object eval_undef(varlisp::Environment& env, const varlisp::List& args)
         SSS_POSITION_THROW(std::runtime_error,
                            "(", funcName, ": cannot undef keywords", p_sym->name(), ")");
     }
+    detail::json_accessor jc{p_sym->name()};
     bool ret = false;
-    // FIXME
-    Object* it = env.deep_find(p_sym->name());
-    if (it) {
-        const varlisp::Builtin * p_b = boost::get<varlisp::Builtin>(&(*it));
-        if (p_b) {
-            SSS_POSITION_THROW(std::runtime_error,
-                               "(", funcName, ": cannot undef builtin function)");
+    auto location = detail::json_accessor::locate(env, *p_sym);
+    if (!location.first) {
+        SSS_POSITION_THROW(std::runtime_error,
+                           "(", funcName, ": tobe delete symbol ", p_sym->name(), " not exist)");
+    }
+    if (jc.has_sub()) {
+        if (!sss::is_all(jc.stems().back(), static_cast<int(*)(int)>(std::isdigit))) {
+            location.second->erase(jc.stems().back());
+            ret = true;
         }
-        env.erase(p_sym->name());
+        else {
+            std::vector<int> indexer;
+            for (size_t i = jc.stems().size(); i != 0; --i) {
+                if (!sss::is_all(jc.stems()[i - 1], static_cast<int(*)(int)>(std::isdigit))) {
+                    break;
+                }
+                indexer.push_back(sss::string_cast<int>(jc.stems()[i - 1]));
+            }
+            std::reverse(indexer.begin(), indexer.end());
+            COLOG_ERROR(indexer);
+            // TODO
+        }
+    }
+    else {
+        location.second->erase(p_sym->name());
         ret = true;
     }
+
     return ret;
 }
 
@@ -87,7 +107,7 @@ Object eval_ifdef(varlisp::Environment& env, const varlisp::List& args)
 
 REGIST_BUILTIN("var-list", 0, 1, eval_var_list,
                "(var-list) -> int64_t\n"
-               "(var-list env-name) -> int64_t ; TODO");
+               "(var-list env-name) -> int64_t");
 
 /**
  * @brief
@@ -108,8 +128,6 @@ Object eval_var_list(varlisp::Environment& env, const varlisp::List& args)
     if (args.length()) {
         SSS_POSITION_THROW(std::runtime_error,
                            "(", funcName, ": list-environment not implement)");
-        // NOTE TODO 只枚举环境本身！
-        // 这里需要判断，是否是关键字；
     }
     else {
         // 被子环境覆盖了的夫环境变量，不会输出。
@@ -119,7 +137,7 @@ Object eval_var_list(varlisp::Environment& env, const varlisp::List& args)
             for (auto it = p_env->begin(); it != p_env->end(); ++it) {
                 if (outted.find(it->first) == outted.end()) {
                     std::cout << it->first << "\n"
-                        << "\t" << it->second.first
+                        << "\t" << it->second.first << (it->second.second.is_const ? " CONST" : "")
                         << std::endl;
                     outted.insert(it->first);
                 }
@@ -130,23 +148,10 @@ Object eval_var_list(varlisp::Environment& env, const varlisp::List& args)
     return var_count;
 }
 
-REGIST_BUILTIN("let", 2, -1, eval_let,
-               "(let ((symbol expr)...) (expr)...) -> result-of-last-expr");
-
-/**
- * @brief
- *      (let ((symbol expr)...)
- *           (expr)...) -> result-of-last-expr
- *
- * @param[in] env
- * @param[in] args
- *
- * @return
- */
-Object eval_let(varlisp::Environment& env, const varlisp::List& args)
+namespace detail {
+Object let_and_letn_impl(varlisp::Environment& env, const varlisp::List& args,
+                         const char* funcName, bool reuse_inner)
 {
-    const char * funcName = "let";
-
     const varlisp::List * p_sym_pairs = boost::get<varlisp::List>(&detail::car(args));
     COLOG_DEBUG(detail::car(args));
     if (!p_sym_pairs) {
@@ -181,7 +186,9 @@ Object eval_let(varlisp::Environment& env, const varlisp::List& args)
         // NOTE let 貌似可以达到swap两个变量值的效果；
         // 在于 getAtomicValue()的第一个参数是env，而不是inner
         // (let ((a b) (b a)) ...)
-        inner[p_sym->name()] = varlisp::getAtomicValue(env,
+        // 而letn和let的区别，仅在于getAtomicValue的第一个参数，是inner，还是
+        // env;
+        inner[p_sym->name()] = varlisp::getAtomicValue(reuse_inner ? inner : env,
                                                        p_sym_pair->nth(1),
                                                        value);
     }
@@ -191,6 +198,78 @@ Object eval_let(varlisp::Environment& env, const varlisp::List& args)
     }
     
     return result;
+}
+
+} // namespace detail
+
+REGIST_BUILTIN("let", 2, -1, eval_let,
+               "(let ((symbol expr)...) (expr)...) -> result-of-last-expr");
+
+/**
+ * @brief
+ *      (let ((symbol expr)...)
+ *           (expr)...) -> result-of-last-expr
+ *
+ * @param[in] env
+ * @param[in] args
+ *
+ * @return
+ */
+Object eval_let(varlisp::Environment& env, const varlisp::List& args)
+{
+    return detail::let_and_letn_impl(env, args, "let", false);
+}
+
+REGIST_BUILTIN("letn", 2, -1, eval_letn,
+               "letn 可重用版let；后面定义的符号，可以使用前面符号的值\n"
+               "(letn ((symbol expr)...) (expr)...) -> result-of-last-expr\n"
+
+               "(letn\n"
+               "   (x 2\n"
+               "    y (pow x 3)\n"
+               "    z (pow x 4))\n"
+               " (println x)\n"
+               " (println y)\n"
+               " (println z))\n"
+              );
+
+Object eval_letn(varlisp::Environment& env, const varlisp::List& args)
+{
+    return detail::let_and_letn_impl(env, args, "letn", true);
+}
+
+REGIST_BUILTIN("set", 2, 2, eval_set,
+               "; 对符号引用到的变量做修改；如果变量不存在，则报错\n"
+               "(set 'quote-symbal expr) -> value-of-expr");
+
+Object eval_set(varlisp::Environment& env, const varlisp::List& args)
+{
+    const char * funcName = "setq";
+    Object tmpObj;
+    auto * p_quoted_symbol = varlisp::getTypedValue<varlisp::List>(env, args.nth(0), tmpObj);
+    if (p_quoted_symbol && p_quoted_symbol->is_quoted()) {
+        if(auto * p_sym = boost::get<varlisp::symbol>(p_quoted_symbol->unquote())) {
+            if (auto * p_value = env.deep_find(p_sym->name())) {
+                Object res;
+                *p_value = varlisp::getAtomicValue(env, args.nth(1), res);
+                return *p_value;
+            }
+            else {
+                SSS_POSITION_THROW(std::runtime_error, "(", funcName, ": symbol, ",
+                                   *p_sym, " not exist!)");
+            }
+        }
+        else {
+            SSS_POSITION_THROW(std::runtime_error, "(", funcName,
+                               ": need a quoted symbol, as 1st argument, but ",
+                               *p_quoted_symbol, " )");
+        }
+    }
+    else {
+            SSS_POSITION_THROW(std::runtime_error, "(", funcName,
+                               ": need a quoted symbol, as 1st argument, but ",
+                               args.nth(0), " )");
+    }
 }
 
 REGIST_BUILTIN("setq", 2, -1, eval_setq,
@@ -310,6 +389,94 @@ Object eval_swap(varlisp::Environment& env, const varlisp::List& args)
 
     std::swap(*p_val1, *p_val2);
     return varlisp::Nill{};
+}
+
+REGIST_BUILTIN("locate", 2, 2, eval_locate,
+               "; locate 在某{}或者[]，按照json-accessor的语法，返回子对象的值\n"
+               "; 允许字面值\n"
+               "(locate {}-or-[]-value '(symbol-or-int...)) -> sub-value");
+
+// TODO 如何用类似的语法，进行更改值呢？
+Object eval_locate(varlisp::Environment& env, const varlisp::List& args)
+{
+    const char * funcName = "locate";
+    Object tmp;
+    const Object& objRef = varlisp::getAtomicValue(env, detail::car(args), tmp);
+
+    Object tmpStems;
+    auto p_stem_list = varlisp::getQuotedList(env, detail::cadr(args), tmpStems);
+    if (!p_stem_list) {
+        SSS_POSITION_THROW(std::runtime_error,
+                           "(", funcName, ": 2nd argument must be a s-list; but ", detail::cadr(args), " )");
+    }
+
+    auto * p_obj = &objRef;
+    auto * p_env = &env;
+    for (auto stem_it = p_stem_list->begin(); stem_it != p_stem_list->end(); ++stem_it) {
+        if (auto * p_index = boost::get<int64_t>(&*stem_it)) {
+            auto * p_list = boost::get<varlisp::List>(p_obj);
+            if (!p_list) {
+                SSS_POSITION_THROW(std::runtime_error,
+                                   "need a List here , but ", p_obj->which());
+            }
+            p_list = p_list->unquoteType<varlisp::List>();
+            if (!p_list) {
+                SSS_POSITION_THROW(std::runtime_error,
+                                   "need a s-List here , but ", p_obj->which());
+            }
+
+            p_obj = const_cast<varlisp::Object*>(&p_list->nth(*p_index));
+        }
+        else if (auto * p_sym = boost::get<varlisp::symbol>(&*stem_it)) {
+            p_env = const_cast<varlisp::Environment*>(boost::get<varlisp::Environment>(p_obj));
+            if (!p_env) {
+                SSS_POSITION_THROW(std::runtime_error,
+                                   "need an Environment here , but ", *stem_it);
+            }
+            p_obj = p_env->find(p_sym->name());
+        }
+        else {
+            SSS_POSITION_THROW(std::runtime_error,
+                               "(", funcName, ": type error; ", p_obj->which(), " )");
+        }
+    }
+
+    return *p_obj;
+}
+
+REGIST_BUILTIN("symbols", 0, 1, eval_symbols,
+               "; symbols 返回当前，或者目标{}内部的所有标识符symbol列表\n"
+               "(symbols) -> [current-symbol-list]]\n"
+               "(symbols {}-name) -> [target-{}-symbol-list]");
+
+Object eval_symbols(varlisp::Environment& env, const varlisp::List& args)
+{
+    const char * funcName = "symbols";
+    auto * p_env = &env;
+    if (!args.empty()) {
+        Object tmp;
+        const varlisp::symbol* p_sym = varlisp::getSymbol(env, detail::car(args), tmp);
+        if (!p_sym) {
+            SSS_POSITION_THROW(std::runtime_error,
+                               "(", funcName, ": need a symbol at first argument, but ", detail::car(args), ")");
+        }
+        auto location = detail::json_accessor::locate(env, *p_sym);
+        if (!location.first) {
+            SSS_POSITION_THROW(std::runtime_error,
+                               "(", funcName, ": symbol ", *p_sym, " cannot be found)");
+        }
+        p_env = boost::get<varlisp::Environment>(location.first);
+        if (!p_env) {
+            SSS_POSITION_THROW(std::runtime_error,
+                               "(", funcName, ": symbol ", *p_sym, " is not to a context)");
+        }
+    }
+    auto symbols = varlisp::List::makeSQuoteList();
+    auto back_it = detail::list_back_inserter<Object>(symbols);
+    for (auto it = p_env->begin(); it != p_env->end(); ++it) {
+        *back_it++ = std::move(varlisp::symbol(it->first));
+    }
+    return symbols;
 }
 
 } // namespace varlisp
