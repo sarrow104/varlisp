718f7646d44b4e5f7dec5f1c31df3ebb47d69718
sarrow104 <sarrow104@gmail.com>
2016-12-19 11:20:49 +0800

    add cookie search path and add some comments

--- varlisp/detail/varlisp_env.cpp
+++ varlisp/detail/varlisp_env.cpp
@@ -9,7 +9,10 @@ sss::PenvMgr2& get_envmgr()
     static bool init_mgr = [](sss::PenvMgr2& mgr)->bool {
         std::string root = sss::path::dirname(sss::path::getbin());
         mgr.set("root", root);
+        // 脚本默认搜索路径
         mgr.set("root_script", sss::path::append_copy(root, "script"));
+        // cookie默认搜索路径
+        mgr.set("root_cookie", sss::path::append_copy(root, "cookie"));
         return true;
     }(mgr);
     (void)init_mgr;
