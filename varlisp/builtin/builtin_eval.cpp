f5a74dd458c84e3af5c9d6f5020d268caeed6e5e
sarrow104 <sarrow104@gmail.com>
2016-12-20 23:21:10 +0800

    rm some comment

--- varlisp/builtin/builtin_eval.cpp
+++ varlisp/builtin/builtin_eval.cpp
@@ -32,9 +32,6 @@ Object eval_eval(varlisp::Environment& env, const varlisp::List& args)
         return refObj;
     }
     // NOTE 对于(eval (quote atomic))，返回atomic
-
-    // SSS_POSITION_THROW(std::runtime_error,
-    //                    "(", funcName, ": need squote-List as 1st argument)");
 }
 
 REGIST_BUILTIN("eval-string", 1, 2, eval_eval_string, "(eval-string \"(+ 1 2)\") -> ...");
