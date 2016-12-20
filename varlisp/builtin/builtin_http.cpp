b6243c6ffaa1a6a30c36eea5781b78401dd0e6be
sarrow104 <sarrow104@gmail.com>
2016-12-20 23:27:19 +0800

    refactory: use requireTypedValue() instead;

--- varlisp/builtin/builtin_http.cpp
+++ varlisp/builtin/builtin_http.cpp
@@ -1,5 +1,7 @@
 #include <cctype>
 
+#include <array>
+
 #include <boost/asio.hpp>
 #include <boost/system/error_code.hpp>
 
@@ -40,30 +42,23 @@ REGIST_BUILTIN(
 Object eval_http_get(varlisp::Environment& env, const varlisp::List& args)
 {
     const char* funcName = "http-get";
-    Object url;
-    const string_t* p_url =
-        getTypedValue<string_t>(env, detail::car(args), url);
-    if (!p_url) {
-        SSS_POSITION_THROW(std::runtime_error, "(", funcName,
-                           ": requie downloading url as 1st argument !)");
+    if (args.length() == 2) {
+        SSS_POSITION_THROW(std::runtime_error,
+                           "(", funcName, ": need 1 or 3 parameters)");
     }
+    std::array<Object, 3> objs;
+    const string_t* p_url =
+        requireTypedValue<varlisp::string_t>(env, args.nth(0), objs[0], funcName, 0, DEBUG_INFO);
 
-    Object proxy;
     const string_t* p_proxy = 0;
-    Object port;
     const int64_t* p_port = 0;
 
     if (args.length() == 3) {
-        p_proxy = getTypedValue<string_t>(env, detail::cadr(args), proxy);
-        if (!p_proxy) {
-            SSS_POSITION_THROW(std::runtime_error, "(", funcName,
-                               ": 2nd parameter must be proxy domain string!)");
-        }
-        p_port = getTypedValue<int64_t>(env, detail::caddr(args), port);
-        if (!p_port) {
-            SSS_POSITION_THROW(std::runtime_error, "(", funcName,
-                               ": 3rd parameter must be proxy port number!)");
-        }
+        p_proxy = 
+            requireTypedValue<varlisp::string_t>(env, args.nth(1), objs[1], funcName, 1, DEBUG_INFO);
+
+        p_port = 
+            requireTypedValue<int64_t>(env, args.nth(2), objs[2], funcName, 2, DEBUG_INFO);
     }
 
     ss1x::http::Headers headers;
