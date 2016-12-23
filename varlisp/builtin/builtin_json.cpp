3ed67cac0804d0dcf351b370ad1d6109836af266
sarrow104 <sarrow104@gmail.com>
2016-12-23 21:02:32 +0800

    add (json-indent...); add human-readable json-print mode

--- varlisp/builtin/builtin_json.cpp
+++ varlisp/builtin/builtin_json.cpp
@@ -9,11 +9,13 @@
 #include "../json_print_visitor.hpp"
 #include "../json/parser.hpp"
 #include "../detail/car.hpp"
+#include "../detail/json.hpp"
 
 namespace varlisp {
 
 namespace detail {
-void json_object2stream(std::ostream& o, varlisp::Environment& env, const Object& obj, const char * funcName)
+void json_object2stream(std::ostream& o, varlisp::Environment& env,
+                        const Object& obj, const char* funcName, bool indent)
 {
     Object tmp;
     const Object& objRef = varlisp::getAtomicValue(env, obj, tmp);
@@ -33,33 +35,45 @@ void json_object2stream(std::ostream& o, varlisp::Environment& env, const Object
                            "(", funcName, ": only Environment type or s-list type is valid; but ",
                            objRef, ")");
     }
-    boost::apply_visitor(json_print_visitor(o), objRef);
+    boost::apply_visitor(json_print_visitor(o, indent), objRef);
 }
 
 } // namespace detail
 
-REGIST_BUILTIN("json-print", 1, 1, eval_json_print,
+REGIST_BUILTIN("json-print", 1, 2, eval_json_print,
                "; json-print 用json格式，打印內建对象；\n"
                "; 注意，仅支持list和env\n"
-               "(json-print obj) -> nil");
+               "(json-print obj boolean) -> nil");
 
 Object eval_json_print(varlisp::Environment& env, const varlisp::List& args)
 {
     const char * funcName = "json-print";
-    detail::json_object2stream(std::cout, env, detail::car(args), funcName);
+    bool indent = false;
+    if (args.length() >= 2) {
+        std::array<Object, 1> objs;
+        indent = *requireTypedValue<bool>(env, args.nth(1), objs[0], funcName,
+                                          1, DEBUG_INFO);
+    }
+    detail::json_object2stream(std::cout, env, detail::car(args), funcName, indent);
     return Nill{};
 }
 
-REGIST_BUILTIN("json-string", 1, 1, eval_json_string,
+REGIST_BUILTIN("json-string", 1, 2, eval_json_string,
                "; json-string 用json格式，序列化內建对象；\n"
                "; 注意，仅支持list和env\n"
-               "(json-string obj) -> nil");
+               "(json-string obj boolean) -> nil");
 
 Object eval_json_string(varlisp::Environment& env, const varlisp::List& args)
 {
     const char * funcName = "json-string";
     std::ostringstream oss;
-    detail::json_object2stream(oss, env, detail::car(args), funcName);
+    bool indent = false;
+    if (args.length() >= 2) {
+        std::array<Object, 1> objs;
+        indent = *requireTypedValue<bool>(env, args.nth(1), objs[0], funcName,
+                                          1, DEBUG_INFO);
+    }
+    detail::json_object2stream(oss, env, detail::car(args), funcName, indent);
     return string_t{std::move(oss.str())};
 }
 
@@ -76,6 +90,23 @@ Object eval_json_parse(varlisp::Environment& env, const varlisp::List& args)
         requireTypedValue<string_t>(env, args.nth(0), obj, funcName, 0, DEBUG_INFO);
 
     return json::parse(p_s->to_string_view());
+}
+
+REGIST_BUILTIN("json-indent", 1, 1, eval_json_indent,
+               "; json-indent 管理json的可读打印模式时候，缩进量\n"
+               "(json-indent) -> \"current-json-indent-setting\"\n"
+               "(json-indent \"string\") -> \"json-indent-setting\"");
+
+Object eval_json_indent(varlisp::Environment& env, const varlisp::List& args)
+{
+    const char * funcName = "json-indent";
+    if (args.length()) {
+        std::array<Object, 1> objs;
+        const auto * p_indent =
+            requireTypedValue<string_t>(env, args.nth(0), objs[0], funcName, 0, DEBUG_INFO);
+        varlisp::detail::json::set_json_indent(p_indent->to_string());
+    }
+    return string_t(varlisp::detail::json::get_json_indent());
 }
 
 } // namespace varlisp
