f5fc2b0841459daeea1907f8cfd3936a7db5565e
sarrow104 <sarrow104@gmail.com>
2016-12-23 20:53:04 +0800

    add COLOR_DEBUG()

--- varlisp/eval_visitor.cpp
+++ varlisp/eval_visitor.cpp
@@ -1,6 +1,7 @@
 #include "eval_visitor.hpp"
 
 #include <sss/utlstring.hpp>
+#include <sss/colorlog.hpp>
 
 #include "environment.hpp"
 #include "list.hpp"
@@ -10,10 +11,12 @@ namespace varlisp {
 Object eval_visitor::operator()(const varlisp::symbol& s) const
 {
     Object* it = m_env.deep_find(s.name());
+
     if (!it) {
         SSS_POSITION_THROW(std::runtime_error, "symbol ", s.name(),
                           " not exsist");
     }
+    COLOG_DEBUG(s, *it);
     return *it;
 }
 
