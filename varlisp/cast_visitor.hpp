8cb28d7d710d72881ee3b7829b057f8d71166e26
sarrow104 <sarrow104@gmail.com>
2016-12-28 10:48:55 +0800

    refactory: using varlisp::getAtomicValueUnquote()

--- varlisp/cast_visitor.hpp
+++ varlisp/cast_visitor.hpp
@@ -35,24 +35,35 @@ struct cast_visitor : public boost::static_visitor<Object>
         return Nill{};
     }
 
-    Object operator() (const symbol&, const string_t& s) const
+    // (cast a "my-var") -> my-var
+    // (cast a 'var-b) -> var-b
+    //
+    // NOTE
+    //
+    // (eval 'a) -> cannot find symbol a | nil (newlisp中，未定义的符号值是nil）
+    template<typename T>
+    Object operator() (const symbol&, const T& v) const
     {
-        std::string name = s.to_string_view().to_string();
-        if (detail::is_symbol(name)) {
-            return symbol(name);
+        Object temp;
+        const Object& res = varlisp::getAtomicValueUnquote(m_env, m_value, temp);
+
+        if (auto p_sym = boost::get<varlisp::symbol>(&res)) {
+            return res;
         }
-        else {
-            return Nill{};
+        else if (auto p_string = boost::get<varlisp::string_t>(&res)) {
+            std::string name = p_string->to_string();
+            if (detail::is_symbol(name)) {
+                return symbol(name);
+            }
         }
+        return Nill{};
     }
     template<typename T>
-    Object operator() (const int64_t&, const T& v) const {
+    Object operator() (const int64_t&, const T& v) const
+    {
         Object temp;
-        const Object& res = varlisp::getAtomicValue(m_env, m_value, temp);
-        if (auto * p_bool = boost::get<bool>(&res)) {
-            return int64_t(*p_bool);
-        }
-        else if (auto * p_double = boost::get<double>(&res)) {
+        const Object& res = varlisp::getAtomicValueUnquote(m_env, m_value, temp);
+        if (auto * p_double = boost::get<double>(&res)) {
             if (std::abs(*p_double) > std::numeric_limits<int64_t>::max()) {
                 return Nill{};
             }
@@ -75,9 +86,10 @@ struct cast_visitor : public boost::static_visitor<Object>
         return Nill{};
     }
     template<typename T>
-    Object operator() (const double&, const int64_t& v) const {
+    Object operator() (const double&, const int64_t& v) const
+    {
         Object temp;
-        const Object& res = varlisp::getAtomicValue(m_env, m_value, temp);
+        const Object& res = varlisp::getAtomicValueUnquote(m_env, m_value, temp);
         if (auto * p_bool = boost::get<bool>(&res)) {
             return double(*p_bool);
         }
@@ -99,17 +111,28 @@ struct cast_visitor : public boost::static_visitor<Object>
         return Nill{};
     }
     template<typename T>
-    Object operator() (const string_t, const T& value) const {
+    Object operator() (const string_t, const T& value) const
+    {
+        Object temp;
+        const Object& res = varlisp::getAtomicValueUnquote(m_env, m_value, temp);
+        if (auto * p_string = boost::get<varlisp::string_t>(&res)) {
+            return res;
+        }
+        else if (auto * p_sym = boost::get<varlisp::symbol>(&res)) {
+            return string_t(p_sym->name());
+        }
         std::ostringstream oss;
         boost::apply_visitor(print_visitor(oss), m_value);
         return string_t{std::move(oss.str())};
     }
     template<typename T>
-    Object operator() (const bool&, const T& s) const {
+    Object operator() (const bool&, const T& s) const
+    {
         return boost::apply_visitor(cast2bool_visitor(m_env), m_value);
     }
     //NOTE cast到正则
-    Object operator() (const varlisp::regex_t&, const varlisp::string_t& s) const {
+    Object operator() (const varlisp::regex_t&, const varlisp::string_t& s) const
+    {
         return std::make_shared<RE2>(s.to_string_view().to_string());
     }
     // NOTE
