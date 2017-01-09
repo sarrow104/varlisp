88889c6a594f28b5b0db4d4eba22c934772bc6cc
sarrow104 <sarrow104@gmail.com>
2017-01-09 22:45:55 +0800

    add (range...)

--- varlisp/builtin/builtin_list.cpp
+++ varlisp/builtin/builtin_list.cpp
@@ -237,8 +237,9 @@ Object eval_append(varlisp::Environment& env, const varlisp::List& args)
     }
     varlisp::List ret = varlisp::List();
 
-    ret.append_list(*p_list1);
-    ret.append_list(*p_list2);
+    // NOTE 不能直接append！必须要求值！
+    ret.append_list(env, *p_list1);
+    ret.append_list(env, *p_list2);
 
     return varlisp::List::makeSQuoteObj(ret);
 }
@@ -282,5 +283,124 @@ Object eval_flat(varlisp::Environment& env, const varlisp::List& args)
 
     return varlisp::List::makeSQuoteObj(ret);
 }
+
+namespace detail {
+// TODO start nil -> 0
+// stop nil -> -1
+// step nil -> 1
+std::tuple<int64_t, int64_t, int64_t> range_require_impl(varlisp::Environment& env, const varlisp::List& args, const char *funcName, int index_offset)
+{
+    std::array<Object, 3> objs;
+    std::tuple<int32_t, int32_t, int32_t> range {0, 0, 1};
+
+    std::get<0>(range) =
+        *varlisp::requireTypedValue<int64_t>(env, args.nth(0), objs[0], funcName, 0 + index_offset, DEBUG_INFO);
+
+    std::get<1>(range) =
+        *varlisp::requireTypedValue<int64_t>(env, args.nth(1), objs[1], funcName, 1 + index_offset, DEBUG_INFO);
+
+    if (args.size() >= 3) {
+        std::get<2>(range) =
+            *varlisp::requireTypedValue<int64_t>(env, args.nth(2), objs[2], funcName, 2 + index_offset, DEBUG_INFO);
+
+        if (0 == std::get<2>(range)) {
+            SSS_POSITION_THROW(std::runtime_error, "(", funcName, ", step == zero!)");
+        }
+    }
+
+    return range;
+}
+
+int64_t index_regular(int64_t index, uint32_t length)
+{
+    if (index < 0) {
+        index += int64_t(length);
+    }
+    if (index > int64_t(length)) {
+        index = length;
+    }
+    if (index < 0) {
+        index = 0;
+    }
+    return index;
+}
+
+} // namespace detail
+
+// NOTE python的方括号，切片是按照闭合、开放区间返回的。
+// 另外，如果step为负数，就表示返回一个逆向list；
+// 另外，范围不对的话，会返回一个空list。
+// 然后，正数end，可以大于等于当前list的长度。当然，返回的list，并不会超过原有的list长！
+// 我这里，如何设定？用半开半闭，还是双闭合？
+// 还有，负数的end，又表示什么样的范围呢？
+//
+// 另外，如果为了节省内存的话，可以考虑内嵌一个index列表，来重复引用list。
+// 即，varlisp::List，内部再保存一个optinal<std::vector<int>>的成员，用来保存，
+// 可能的切片结构；
+REGIST_BUILTIN("slice", 3, 4, eval_slice,
+               "; slice 切片截取\n"
+               "; begin,end 都是表示位置的点；如果begin为nil，表示0；"
+               "; end 如果为nil，表示取最后一位\n"
+               "; step 默认是1；\n"
+               "(slice '(list) begin end)\n"
+               "(slice '(list) begin end step)");
+
+Object eval_slice(varlisp::Environment& env, const varlisp::List& args)
+{
+    const char * funcName = "slice";
+    std::array<Object, 1> objs;
+    const auto * p_list = getQuotedList(env, args.nth(0), objs[0]);
+    varlisp::requireOnFaild<varlisp::QuoteList>(p_list, funcName, 0, DEBUG_INFO);
+
+    varlisp::List ret = varlisp::List();
+    int64_t start = 0, stop = -1, step = 1;
+    std::tie(start, stop, step) = detail::range_require_impl(env, args.tail(), funcName, 1);
+    start = detail::index_regular(start, p_list->size());
+    stop = detail::index_regular(stop, p_list->size());
+    for (auto i = start; i >= 0 && i < int64_t(p_list->size()) && i != stop; i += step) {
+        ret.append(p_list->nth(i));
+    }
+    return varlisp::List::makeSQuoteObj(ret);
+}
+
+REGIST_BUILTIN("range", 2, 3, eval_range,
+               "; range 返回一个序列\n"
+               "; 语义，同for\n"
+               "; 实现逻辑，相当于定义一个列表，for内部，就不断往这个列表添加东西\n"
+               "; 或者，干脆用for来实现\n"
+               "; 好像不行；因为我要求range能接受可变参数。而可变参数，必须由解释器来实现……"
+               "(range begin end)\n"
+               "(range begin end step)");
+
+Object eval_range(varlisp::Environment& env, const varlisp::List& args)
+{
+    const char * funcName = "range";
+
+    varlisp::List ret = varlisp::List();
+    int64_t start = 0, stop = -1, step = 1;
+    std::tie(start, stop, step) = detail::range_require_impl(env, args, funcName, 0);
+    auto offset = stop - start;
+    auto times = offset / step;
+    if (times < 0) {
+        SSS_POSITION_THROW(std::runtime_error, "(", funcName, ", wrong range:", start, stop, step, ")");
+    }
+    for (auto i = 0; i <= times && start + step * i != stop; ++i) {
+        ret.append(start + step * i);
+    }
+    return varlisp::List::makeSQuoteObj(ret);
+}
+
+// TODO reverse reverse也可以用for来实现……
+// (define (reverse l)
+//  (letn ((ret-l []))
+//   (format 1 "{}\n" l)
+//   (for (item l)
+//    (format 1 "{}\n" item)
+//    (setq ret-l (cons item ret-l))
+//   )
+//  )
+// )
+// inter-set 交集
+// outter-set 并集
 
 }  // namespace varlisp
