a475237646c1fd648f8f662046d533f1b1a9f208
sarrow104 <sarrow104@gmail.com>
2021-04-15 15:14:38 +0800

    add register_fd(int fd), unregister_fd(int fd) to support auto remove temp file when closing

--- varlisp/detail/file.cpp
+++ varlisp/detail/file.cpp
@@ -6,9 +6,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <map>
 #include <stdexcept>
 
 #include <sss/util/PostionThrow.hpp>
+#include <sss/path.hpp>
 
 namespace varlisp {
 namespace detail {
@@ -46,6 +48,58 @@ std::string get_fname_from_fd(int fd)
         }
     }
     return name;
+}
+
+static void on_exit();
+
+typedef std::map<int, std::string> registered_fd_path_map_type;
+
+registered_fd_path_map_type& get_fd_path_map()
+{
+    static registered_fd_path_map_type fd_path_map;
+    static bool reg_on_exit = []() -> bool {
+        return std::atexit(on_exit) != 0;
+    }();
+    (void) reg_on_exit;
+    return fd_path_map;
+}
+
+bool register_fd(int fd)
+{
+    std::string path;
+    try {
+        path = get_fname_from_fd(fd);
+        return get_fd_path_map().insert(std::make_pair(fd, path)).second;
+    }
+    catch (...) {
+        return false;
+    }
+}
+
+bool unregister_fd(int fd)
+{
+    auto& m = get_fd_path_map();
+    auto it = m.find(fd);
+    if (it == m.end()) {
+        return false;
+    } else {
+        auto path = it->second;
+        m.erase(it);
+        sss::path::remove(path);
+
+        return true;
+    }
+}
+
+static void on_exit()
+{
+    auto& m = get_fd_path_map();
+    auto it = m.begin();
+    while (it != m.end()) {
+        auto path = it->second;
+        it = m.erase(it);
+        sss::path::remove(path);
+    }
 }
 
 } // namespace file
